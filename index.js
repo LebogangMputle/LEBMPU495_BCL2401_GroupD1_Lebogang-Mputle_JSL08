6e795f0b8e991833e9c4af42def420006a1cbd65
LebogangMputle <lebozikalala@gmail.com>
2024-04-09 16:26:53 +0200

    Define methods to manage branch information

--- index.js
+++ index.js
@@ -47,10 +47,19 @@ if (!bankBranchInstance) {
 }
 return bankBranchInstance;
 }
-}
+
 
 // Define methods to manage branch information
-function getBranchInfo() {
+getBranchInfo() {
     return this.branchInfo;
 }
+// Other methods related to branch management can be added here
+}
 
+//Usage
+const branchA = new BankBranch("Main Street Branch");
+console.log(branchA.getBranchInfo()); // Outputs: Main Street Branch
+const branchB = new BankBranch("Second Street Branch");
+ 
+console.log(branchB.getBranchInfo()); // Still outputs: Main Street Branch, because it's a singleton
+console.log(branchA === branchB); // true, both variables point to the same instance
